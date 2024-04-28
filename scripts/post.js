2fade2371cb1b7d8691bf381ed2923467146f81d
Miguel Mañabo <132088504+Fourthhy@users.noreply.github.com>
2024-04-29 02:45:58 +0800

    Update post.js

--- scripts/post.js
+++ scripts/post.js
@@ -38,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function() {
 function backLogin() {
     document.getElementById("login").addEventListener("click", (e)=> {
         setTimeout(()=> {
-            window.location.href = "login.html"
+            window.location.href = "index.html"
         }, 500);
         alert("Returning to Login");
     });
